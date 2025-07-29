e1912c76d9e9cc5d05d76c0d54adad5829e470e7
Lucas <Lukinhasx248@gmail.com>
2025-07-29 14:06:47 -0300

    ta dando errado

---
+++ 3Bimestre/LojaVirtual/model/Loja.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Loja {
+    private String nome;
+    private ArrayList<Produto> produtos;
+
+    public Loja() {
+        this.nome = null;
+        this.produtos = new ArrayList<Produto>();
+    }
+
+    public Loja(String nome) {
+        this.nome = nome;
+        this.produtos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(ArrayList<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+    }
+
+    public void removerProduto(Produto produto) {
+        produtos.remove(produto);
+    }
+
+    @Override
+    public String toString() {
+        return "Loja [nome=" + nome + ", produtos=" + produtos + "]";
+    }
+
+
+}
